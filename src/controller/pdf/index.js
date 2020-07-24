7a4775cc225a4903aa345398daa9383b89b3d442
caoshiyuan <codesircao@126.com>
2020-07-24 16:01:10 +0800

    数据处理

--- src/controller/pdf/index.js
+++ src/controller/pdf/index.js
@@ -18,19 +18,63 @@ class PDF {
         let file = ctx.request.files['file']
         let loadPDFR = await this.loadPDF(file)
         if (!loadPDFR) return { code: 500, msg: '加载pdf失败' }
-        let { allRows, rows, arr } = loadPDFR
-        const data = this.generatorWord(arr)
+        let { allRows, rows, arr, obj } = loadPDFR
+        const data = this.generatorWord(arr, allRows)
         const error = await this.writeXLXS(data, file.name)
         if (error) return { code: 500, error }
         // ctx.set('Content-Type', 'application/vnd.openxmlformats')
         // ctx.set('Content-Disposition', 'attachment; filename=' + 'Report.xlsx')
         return {
             code: 200,
+            allRows,
+            rows,
+            arr,
+            obj,
             url: `http://${ctx.headers.host}/jiaban/${file.name}.xlsx`
         }
         // return { code: 200, allRows, rows }
     }
-    generatorWord(rows) {
+    getRows(text, allRows) {
+        let oRow = {}
+        for (const key in allRows) {
+            if (allRows.hasOwnProperty(key)) {
+                let row = allRows[key]
+                if (row['R']) {
+                    let content = row['R'].map(v => v.T).join()
+                    content = content ? content.replace(/ /g, '') : ''
+                    text = text ? text.replace(/ /g, '') : ''
+                    if (content == text) {
+                        Object.assign(oRow, {
+                            x: row.x,
+                            y: row.y,
+                            text: content
+                        })
+                    }
+                }
+            }
+        }
+        if (oRow.y) {
+            const allKeys = Object.keys(allRows).filter(
+                v => v.split('_')[0] == oRow.y
+            )
+            oRow.contentArr = allKeys.map(v => {
+                const current = allRows[v]
+                return {
+                    x: current.x,
+                    y: current.y,
+                    text: (current['R'] || []).map(j => j.T).join()
+                }
+            })
+        }
+        return oRow
+    }
+    generatorWord(rows, allRows) {
+        const nameRow = this.getRows('申请人', allRows)
+        const name = nameRow
+            ? nameRow['contentArr'][1]
+                ? nameRow['contentArr'][1]['text']
+                : ''
+            : ''
         let cols = [
             '姓名',
             '日期',
@@ -44,18 +88,22 @@ class PDF {
         ]
         let conf = {
             name: 'mysheet',
-            cols: cols.map(v => {
+            cols: cols.map((v, i) => {
                 return {
                     caption: v,
-                    type: 'string'
-                    // beforeCellWrite: function(row, cellData) {
-                    //     return cellData.toUpperCase()
-                    // }
-                    // width: 28.7109375
+                    type: v == '餐费' ? 'number' : 'string',
+                    beforeCellWrite: function(row, cellData) {
+                        return cellData || ''
+                    },
+                    width: v == '日期' || v == '加班时间' ? 40 : 20
                 }
             }),
-            rows
+            rows: rows.map(v => {
+                v[0] = name || v[0]
+                return v
+            })
         }
+        console.log('申请人', nameRow)
         let data = nodeExcel.execute(conf)
         return data
         // return new Buffer(data, 'binary')
@@ -99,19 +147,33 @@ class PDF {
                             v => v.split('_')[0] == row.y
                         )
                         row.contentArr = allKeys.map(v => {
+                            const current = allRows[v]
                             return {
-                                text: (allRows[v]['R'] || [])
-                                    .map(j => j.T)
-                                    .join()
+                                x: current.x,
+                                y: current.y,
+                                text: (current['R'] || []).map(j => j.T).join()
                             }
                         })
+                        const hours = parseFloat(row.contentArr[3]['text']) //加班时长
+                        const beginDate = row.contentArr[1]['text']
+                        const endDate = row.contentArr[2]['text']
+                        const day = beginDate.slice(0, 10) //加班日期
+                        const beginTime = beginDate.slice(-5) //加班开始时间
+                        const endTime = endDate.slice(-5) //加班开始时间
+                        row.contentArr[1]['text'] = String(
+                            day.replace(/-/g, '/')
+                        )
+                        row.contentArr[2]['text'] = beginTime + '-' + endTime
+                        row.contentArr[4]['text'] = String(
+                            hours > 1 ? (hours >= 8 ? 40 : 20) : 0
+                        )
                     }
                 }
                 let arr = Object.keys(rows).map(v => {
                     return rows[v]['contentArr'].map(j => j.text)
                 })
 
-                resolve({ allRows, rows, arr })
+                resolve({ allRows, rows, arr, obj })
             })
             pdfParser.loadPDF(file.path)
         })
